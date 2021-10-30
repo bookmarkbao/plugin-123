9dd917843abceac9c9f93445e6b781135630fe86
xiangjun <bookmarkbao@gmail.com>
2021-10-30 15:26:29 +0800

    remove console

--- packages/my-unplugin/playground/tt.ts
+++ packages/my-unplugin/playground/tt.ts
@@ -1,2 +1,2 @@
 // eslint-disable-next-line no-console
-console.log('tt.ts')
+// console.log('tt.ts')
--- packages/my-unplugin/src/index.ts
+++ packages/my-unplugin/src/index.ts
@@ -5,12 +5,12 @@ export default createUnplugin<Options>((options: any) => ({
   name: 'unplugin-starter',
   transformInclude(id) {
     // eslint-disable-next-line no-console
-    console.log('id: ', id)
+    // console.log('id: ', id)
     return id.endsWith('main.ts') || id.endsWith('.ts') || id.endsWith('.js')
   },
   transform(code) {
     // eslint-disable-next-line no-console
-    console.log(code)
+    // console.log(code)
     return code.replace('__UNPLUGIN__', `Hello Unplugin! ${options}`).replace('{@author}', 'xiangjun9988')
   },
 }))

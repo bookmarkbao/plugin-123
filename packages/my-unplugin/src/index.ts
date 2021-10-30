1b5d42d6f023d3373608ef8a3ddfd095162a1419
xiangjun <bookmarkbao@gmail.com>
2021-10-30 15:40:53 +0800

    add .vue

--- packages/my-unplugin/src/index.ts
+++ packages/my-unplugin/src/index.ts
@@ -1,3 +1,10 @@
+/*
+ * @Descripttion:
+ * @Author: xiangjun
+ * @Date: 2021-09-09 13:23:16
+ * @LastEditors: sueRimn
+ * @LastEditTime: 2021-10-30 15:40:45
+ */
 import { createUnplugin } from 'unplugin'
 import { Options } from './types'
 
@@ -6,7 +13,7 @@ export default createUnplugin<Options>((options: any) => ({
   transformInclude(id) {
     // eslint-disable-next-line no-console
     // console.log('id: ', id)
-    return id.endsWith('main.ts') || id.endsWith('.ts') || id.endsWith('.js')
+    return id.endsWith('main.ts') || id.endsWith('.ts') || id.endsWith('.js') || id.endsWith('.vue')
   },
   transform(code) {
     // eslint-disable-next-line no-console

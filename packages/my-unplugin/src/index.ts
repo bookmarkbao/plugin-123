3f371ddcad17aa7dc9cf846b9495b38af7f5cb2f
xiangjun <bookmarkbao@gmail.com>
2021-10-30 14:57:06 +0800

    feat dev plugin for vite , webpack , rollup

---
+++ packages/my-unplugin/src/index.ts
@@ -0,0 +1,16 @@
+import { createUnplugin } from 'unplugin'
+import { Options } from './types'
+
+export default createUnplugin<Options>((options: any) => ({
+  name: 'unplugin-starter',
+  transformInclude(id) {
+    // eslint-disable-next-line no-console
+    console.log('id: ', id)
+    return id.endsWith('main.ts') || id.endsWith('.ts') || id.endsWith('.js')
+  },
+  transform(code) {
+    // eslint-disable-next-line no-console
+    console.log(code)
+    return code.replace('__UNPLUGIN__', `Hello Unplugin! ${options}`).replace('{@author}', 'xiangjun9988')
+  },
+}))
